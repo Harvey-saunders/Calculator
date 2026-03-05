6b00a2bef8ac6fe9f4ecc19797d7e212c5ae1a8e
Harvey Saunders <harvey.saunders217@outlook.com>
2026-03-05 21:14:57 +0000

    main method can now take input and add

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,17 +2,36 @@ import java.util.ArrayList;
 
 public class Calculator {
     
-    
+    private static TextInput reader;
+    private static Adder adder;
 
     public static void main(String[] args) {
-        TextInput reader = new TextInput();
-        ArrayList<Calculation> test = reader.getEquation();
         
-        for(Calculation calc : test){
-            calc.printValues();
+        startUp();
+
+        
+        ArrayList<Calculation> calList = reader.getEquation();
+        Calculation holder = calList.get(0);
+        
+        for(int x = 1; x < calList.size(); x++){
+            Calculation calc = calList.get(x);
+            switch (holder.getOperator()) {
+                case "add":
+                    holder = new Calculation(calc.getOperator(), adder.adding(holder.getValue(), calc.getValue()));
+                    holder.printValues();
+                    holder.printDecimal();
+                    break;
+            
+                default:
+                    System.out.println("error, no operand: " + calc.getOperator());
+                    break;
+            }
         }
-        
-        
+    }
+
+    private static void startUp(){
+        reader = new TextInput();
+        adder = new Adder();
     }
 
 }
